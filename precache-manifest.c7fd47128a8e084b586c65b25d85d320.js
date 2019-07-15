17adcde52bcab267bdb1dd85a5b677153f9db806
hosseinbeig <h.beig@yahoo.com>
2019-07-15 21:11:27 +0800

    Updates

---
+++ precache-manifest.c7fd47128a8e084b586c65b25d85d320.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897d66f00e78e28b2aaa85dcea47611e",
+    "url": "/tasklist_draggable/index.html"
+  },
+  {
+    "revision": "5c3f844f96b7191b7244",
+    "url": "/tasklist_draggable/static/css/main.0d0b0d5c.chunk.css"
+  },
+  {
+    "revision": "24709b9d93878265ee6d",
+    "url": "/tasklist_draggable/static/js/2.4f233275.chunk.js"
+  },
+  {
+    "revision": "5c3f844f96b7191b7244",
+    "url": "/tasklist_draggable/static/js/main.5317e724.chunk.js"
+  },
+  {
+    "revision": "ad3207450c6e601a1ec1",
+    "url": "/tasklist_draggable/static/js/runtime~main.fdd051e3.js"
+  }
+]);
